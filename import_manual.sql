79567a9b3de0d69f7a28a66a605fb300881c709c
abiww1920 <abipebriana17@gmail.com>
2025-11-27 15:24:02 +0700

    Cleanup: Hapus file dokumentasi dan temporary saja

--- import_manual.sql
+++
@@ -1,6 +0,0 @@
--- Script untuk membuat database dan mengimpor data
-CREATE DATABASE IF NOT EXISTS sippres CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE sippres;
-
--- Import struktur dan data dari file sippres.sql
-SOURCE public/sippres.sql;
--- sync_user_siswa.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Menjalankan sinkronisasi User-Siswa...
-php artisan db:seed --class=SinkronisasiUserSiswaSeeder
-echo.
-echo Sinkronisasi selesai!
-pause
